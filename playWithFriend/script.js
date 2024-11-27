d359e33d9af5a0338951c75c89b6cdeaba9caf4b
lida969 <126264906+lida969@users.noreply.github.com>
2024-11-27 14:03:28 +0300

    Update script.js

--- playWithFriend/script.js
+++ playWithFriend/script.js
@@ -293,8 +293,8 @@ function toggleTournamentMode(isTournamentMode) {
     // Показать настройки турнира и счётчики
     scoreElements.style.display = 'block';
     tournamentSettings.style.display = 'block';
-    player1Wins = savedGameData.player1Wins || 0;
-    player2Wins = savedGameData.player2Wins || 0;
+    player1Wins = savedGameData.player1Wins //|| 0;
+    player2Wins = savedGameData.player2Wins //|| 0;
     player1WinsElement.innerText = player1Wins;
     player2WinsElement.innerText = player2Wins;
   } else {
