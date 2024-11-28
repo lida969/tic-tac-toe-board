f5b50c1886e39a3c945bfa2ba1bd02b516735053
lida969 <126264906+lida969@users.noreply.github.com>
2024-11-28 23:32:52 +0300

    Update jsonbin.js

--- jsonbin.js
+++ jsonbin.js
@@ -1,22 +1,26 @@
 // jsonbin.js
+// Идентификатор JSONBin хранилища
+const binId = '6745d217e41b4d34e45af6f2'; 
 
-const binId = '6745d217e41b4d34e45af6f2'; // Замените на ваш JSONBin ID
-const secretKey = '$2a$10$Xj8xmhXt3sGHC6N8xdo3E.F.Xk.BZpSOOo.FDAwNg8GVGbwKX3Xa6'; // Ваш секретный ключ (не кодировать)
+// Секретный ключ для доступа к API JSONBin
+const secretKey = '$2a$10$Xj8xmhXt3sGHC6N8xdo3E.F.Xk.BZpSOOo.FDAwNg8GVGbwKX3Xa6'; 
 
+// Заголовки для запросов
 const headers = {
-  'Content-Type': 'application/json',
-  'X-Master-Key': secretKey, // Используйте ключ без кодирования
+  'Content-Type': 'application/json', // Формат данных — JSON
+  'X-Master-Key': secretKey, // Секретный ключ для авторизаци
 };
 
-// Сохранение данных игры в JSONBin
+// Функция для сохранения данных игры в JSONBin
 export const saveGame = async (gameData) => {
   try {
+      // Отправка запроса на сохранение данных
     const response = await fetch(`https://api.jsonbin.io/v3/b/${binId}`, {
-      method: 'PUT',
+      method: 'PUT', // Обновление данных
       headers: headers,
-      body: JSON.stringify(gameData),
+      body: JSON.stringify(gameData), // Данные игры в формате JSON
     });
-
+ // Проверка успешности запроса
     if (response.ok) {
       console.log('Game data saved successfully!');
     } else {
@@ -27,18 +31,19 @@ export const saveGame = async (gameData) => {
   }
 };
 
-// Загрузка данных игры из JSONBin
+// Функция для загрузки данных игры из JSONBin
 export const loadGame = async () => {
   try {
+    // Отправка запроса для получения данных
     const response = await fetch(`https://api.jsonbin.io/v3/b/${binId}/latest`, {
-      method: 'GET',
+      method: 'GET', // Получение данных
       headers: headers,
     });
-
+// Проверка успешности запроса
     if (response.ok) {
       const { record } = await response.json();
       console.log('Loaded game data:', record);
-      return record;
+      return record; // Возврат данных
     } else {
       console.error('Failed to load game data:', response.statusText);
     }
